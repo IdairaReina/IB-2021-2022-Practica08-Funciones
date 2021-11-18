630418445d05f969aec718c0e42c4a1a8eb7dd9d
IdairaReina <alu0101398495@ull.edu.es>
2021-11-18 21:29:14 +0000

    asi lo quiere jutge

--- ej-02-Divisors-in-order/divisors_in_order.cc
+++ ej-02-Divisors-in-order/divisors_in_order.cc
@@ -13,36 +13,32 @@
 #include <iostream>
 
 // Declaracion de funciones
-void GetArray(int vector_numbers[]);
-void NumberDividers(int vector_numbers[]);
+void NumberDividers(int number);
 
 int main () {
-  int vector_numbers[6];
-  std::cout << "Este programa imprime en orden todos los divisores de 6 números dados" << std::endl;
-  std::cout << "Introduce los 6 números: " << std::endl;
-  GetArray(vector_numbers);
-  NumberDividers(vector_numbers);
+  std::string number;
+  std::cout << "Este programa imprime en orden todos los divisores los números dados" << std::endl;
+  std::cout << "Introduce los números: " << std::endl;
+  while (std::getline(std::cin, number)) {
+    if (number.empty()) {
+      break;
+    } else {
+      NumberDividers(stoi(number));
+      std::cout << std::endl;
+    }
+  }
   return 0;
 }
 
 // Definicion de funciones
-void GetArray(int vector_numbers[]) {
-  for (int i=0; i<6; i++) {
-    std::cout << "número " << i + 1 << ':' ;
-    std::cin >> vector_numbers[i];
-  }
-}
 
-void NumberDividers(int vector_numbers[]) {
-  int rest;
-  for (int j=0; j < 6; j++) {
-    std::cout << "divisors of " << vector_numbers[j] << ": ";
-    for (int i=1; i<= vector_numbers[j]; i++) {
-      rest = vector_numbers[j] % i;
-      if (rest == 0) {
-        std::cout << i << " ";
-      }
+void NumberDividers(int number) {
+  int rest; 
+  std::cout << "divisors of " << number << ": ";
+  for (int i = 1; i<= number; i++) {
+    rest = number % i;
+    if (rest == 0) {
+      std::cout << i << " ";
     }
-    std::cout << std::endl;
   }
 }
