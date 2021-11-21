b1135e40b8563740ce3f58589184e83eb06856b9
IdairaReina <alu0101398495@ull.edu.es>
2021-11-21 11:46:23 +0000

    mejorada

--- ej-09-Approximation-of-e/approximation_of_e.cc
+++ ej-09-Approximation-of-e/approximation_of_e.cc
@@ -16,45 +16,43 @@
   */
 
 #include <iostream>
-#include <cmath>       /* pow */
 #include <iomanip> 
 
 // Declaración de funciones
-int Factorial(int number);
-double TaylorSeries(int number);
+double TaylorSeries(double number);
 
 int main () {
   std::string number;
+  double result;
   std::cout << "Este programa calcula la serie de Taylor." << std::endl;
   std::cout << "Introduce un número en el intervalo [0-20]: ";
   while (std::getline(std::cin, number)) {
     if (number.empty()) {
       break;
     } else {
+      result = TaylorSeries(stoi(number));
       std::cout << "With " << number << " term(s) we get ";
-      std::cout << std::fixed << std::setprecision(10) << TaylorSeries(stoi(number)) << '.' << std::endl;
+      std::cout << std::fixed << std::setprecision(10) << result << '.' << std::endl;
     }
   }
   return 0;
 }
 
 // Definicion de funciones
-int Factorial(int number) {
-  int result= 1;
-  for (int i=1; i<=number; i++) {
-    result*=i;
+double TaylorSeries(double number){
+  double factorial = 1, division, add = 0;
+  if (number == 0){
+    add = 0;
   }
-  return result;
-}
-
-double TaylorSeries(int number) {
-  double result = 0;
-  if (number == 0) {
-    result = 0;
-  } else {
-    for(int i = 0;i < number; i++){
-      result += (double)1/Factorial(i);
+  else{
+    for (int i = 1; i <= number; i++){
+      for (int j = 1; j <= i; j++){
+        factorial *= j;
+      }
+      division = i/factorial;
+      add += division;
+      factorial = 1;
     }
   }
-  return result;
+  return add;
 }
